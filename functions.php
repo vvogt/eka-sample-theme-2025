fc9d8b9b924f7df4c202ffc8774656a02561bb10
Vahur Vogt <43129530+vvogt@users.noreply.github.com>
2025-02-08 14:13:35 +0200

    Added functions.php and + changed header and footer

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Enqueues scripts and styles.
+ */
+add_action(
+    'wp_enqueue_scripts',
+    function () {
+        wp_enqueue_style('main-style', get_theme_file_uri('style.css'), array(), filemtime(get_theme_file_path('style.css')), 'screen');
+        /* wp_enqueue_script('main-script', get_theme_file_uri('dist/main.js'), array(), filemtime(get_theme_file_path('dist/main.js')), true); */
+    }
+);
+
+?>

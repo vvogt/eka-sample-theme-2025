02b4de42fe9f809e2fa4dcaa014d88d96a228cbe
Vahur Vogt <43129530+vvogt@users.noreply.github.com>
2025-02-08 14:29:22 +0200

    Added reset.css

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ add_action(
     'wp_enqueue_scripts',
     function () {
         wp_enqueue_style('main-style', get_theme_file_uri('style.css'), array(), filemtime(get_theme_file_path('style.css')), 'screen');
+        wp_enqueue_style('reset-style', get_theme_file_uri('reset.css'), array(), filemtime(get_theme_file_path('reset.css')), 'screen');
         /* wp_enqueue_script('main-script', get_theme_file_uri('dist/main.js'), array(), filemtime(get_theme_file_path('dist/main.js')), true); */
     }
 );
--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
 </head>
-<body <?php body_class(''); ?>>
+<body <?php body_class(); ?>>
 
 <header>
     

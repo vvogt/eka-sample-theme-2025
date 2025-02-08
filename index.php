f831cdea31f5c502075aa90788ecb393b47fb556
Vahur Vogt <43129530+vvogt@users.noreply.github.com>
2025-02-08 12:47:13 +0200

    Initial commit
    Created first theme files

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+ get_header();
+
+ $top_section_title = get_field('top_section_title');
+ 
+?>
+
+<main>
+    
+    <h1>
+        <?php echo $top_section_title; ?>
+    </h1>
+    
+
+    <div class="">
+        
+    </div>
+
+</main>
+
+<?php
+ get_footer();
+?>

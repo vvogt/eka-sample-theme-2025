68ccc76a9dec3f47c6d355fe152363410b083889
Vahur Vogt <43129530+vvogt@users.noreply.github.com>
2025-02-15 11:50:13 +0200

    Added hero block

--- front-page.php
+++ front-page.php
@@ -2,13 +2,17 @@
  get_header();
 
  $top_section_title = get_field('top_section_title');
+ $top_section_image = get_field('top_section_background_image');
  
 ?>
 
 <main>
     
     <?php
-        get_template_part('template-parts/hero', null, array('title' => 'Tere tulemast'));
+        get_template_part('template-parts/hero', null, array(
+            'title' => $top_section_title,
+            'image' => $top_section_image,
+        ));
     ?>
 
     <div class="">
--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,7 +1,15 @@
 <?php
     $title = $args['title'];
+    $image = $args['image'];
 ?>
 
 <section class="front-page-hero">
-    <h1><?php echo $title; ?></h1>
+    <div class="content-container">
+        <h1><?php echo $title; ?></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero debitis minus deserunt unde magnam ipsum modi voluptas quas sequi odio? Reprehenderit dolorum nulla esse ut deleniti nemo aut, alias sint!
+        Facilis quidem aperiam autem minima. Earum reiciendis in libero repellendus nobis iure illum perferendis eaque corrupti, non molestias debitis impedit at accusantium pariatur amet blanditiis nisi inventore placeat fuga ex.
+        Sequi earum id eius quo aperiam, porro neque adipisci? Ab maxime necessitatibus, omnis odio voluptatem voluptas quod iure doloremque fugiat suscipit officia nam sequi cum animi nesciunt praesentium reprehenderit quaerat?
+        Corrupti, voluptates tenetur saepe officia obcaecati laudantium alias eos eveniet sequi, id neque cum impedit fugit aspernatur repellendus, in consequatur recusandae eligendi doloremque. Facilis voluptatem neque iure sapiente culpa commodi!</p>
+    </div>
+    <?php echo wp_get_attachment_image($image, 'full', false, array('class' => 'hero-image')); ?>
 </section>
